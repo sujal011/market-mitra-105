ae99b3fb2b87d20775b14d11086013f05c05f78c
Sujal Bhavsar <sujalofficial011@gmail.com>
2024-09-17 17:03:27 +0530

    slider fixed

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,10 +1,10 @@
-import { View, Text, FlatList, Image, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, FlatList, Image, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { config, databases } from '../../lib/appwrite-config';
 import Colors from '../../constants/Colors';
+import { openURL } from 'expo-linking';
 
 const Slider = () => {
-
     const [sliderList, setSliderList] = useState([]);
 
     useEffect(() => {
@@ -16,8 +16,14 @@ const Slider = () => {
             config.databaseId,
             config.sliderCollectionId
         );
+        console.log(response.documents);
         setSliderList(response.documents);
+        
     };
+
+    const openLink = (url)=>{
+        openURL(url)
+    }
 
 
   return (
@@ -30,16 +36,21 @@ const Slider = () => {
         data={sliderList}
         showsHorizontalScrollIndicator={false}
         renderItem={({item,index})=>(
-            <View style={{
+            <TouchableOpacity 
+            onPress={()=>{
+                openLink(item.link)
+            }}
+            style={{
                 borderWidth:1,
                 height:"100%",
                 borderRadius:10,
-                backgroundColor:Colors.BG
+                marginRight:'1%'
+                
             }}>
                 <Image source={{uri:item?.image_url}}
                 style={styles?.slider} />
             
-            </View>
+            </TouchableOpacity>
         )}
       />
     </View>
@@ -50,7 +61,7 @@ export default Slider
 
 const styles = StyleSheet.create({
     slider:{
-        width:Dimensions.get('screen').width*0.83,
+        width:Dimensions.get('screen').width*0.80,
         height:'100%',
         borderRadius:15,
         marginRight:15
